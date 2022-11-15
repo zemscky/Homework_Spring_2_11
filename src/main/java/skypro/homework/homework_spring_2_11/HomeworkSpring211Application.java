6d7296b2789470ac5562a3328e8d96ba68f6cd8f
zemscky <aleksei.zemscky@yandex.ru>
2022-11-15 19:16:55 +0300

    Initial commit

---
+++ src/main/java/skypro/homework/homework_spring_2_11/HomeworkSpring211Application.java
@@ -0,0 +1,13 @@
+package skypro.homework.homework_spring_2_11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkSpring211Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkSpring211Application.class, args);
+    }
+
+}
---
+++ src/test/java/skypro/homework/homework_spring_2_11/HomeworkSpring211ApplicationTests.java
@@ -0,0 +1,13 @@
+package skypro.homework.homework_spring_2_11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkSpring211ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
